bf4b36a19cdf8b3372b376c4fad16d1534c7e3d8
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-03-19 12:17:54 -0700

    we have models

--- orms/orms/settings.py
+++ orms/orms/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our apps
+    'django_extensions',
+    'logs',
+
 ]
 
 MIDDLEWARE_CLASSES = [
